10a1cc01acebc4327f5b072c6604af8fcac70707
Sterben <aaron04cruz12@gmail.com>
2024-06-14 18:06:31 +0800

    Add files for mixed_message project

---
+++ quotes.js
@@ -0,0 +1,88 @@
+const selfImprovementQuotes = [
+  "Believe in yourself!",
+  "Every day is a second chance",
+  "The harder you work for something, the greater you’ll feel when you achieve it",
+  "The only way to do great work is to love what you do",
+  "Strive not to be a success, but rather to be of value",
+  "The best way to predict the future is to create it",
+  "Your life does not get better by chance, it gets better by change",
+  "Believe you can and you're halfway there",
+  "Don't watch the clock; do what it does Keep going",
+  "Act as if what you do makes a difference It does" 
+];
+
+const heartBrokenQuotes = [
+  "The saddest thing about love is that not only the love cannot last forever, but even the heartbreak is soon forgotten",
+  "It is better to have loved and lost than never to have loved at all",
+  "Sometimes good things fall apart so better things can fall together",
+  "The emotion that can break your heart is sometimes the very one that heals it" ,
+  "Hearts will never be practical until they are made unbreakable",
+  "The heart was made to be broken" ,
+  "I have lost and loved and won and cried myself to the person I am today" ,
+  "The cure for a broken heart is simple, my lady A hot bath and a good night's sleep",
+  "Don't cry when the sun is gone, because the tears won't let you see the stars",
+  "It’s hard asking someone with a broken heart to fall in love again"
+];
+
+const lonelyQuotes = [
+  "The eternal quest of the human being is to shatter his loneliness",
+  "Loneliness and the feeling of being unwanted is the most terrible poverty",
+  "The greatest thing in the world is to know how to belong to oneself",
+  "The only time we waste is the time we spend thinking we are alone" ,
+  "The worst loneliness is to not be comfortable with yourself",
+  "Being alone has a power that very few people can handle",
+  "We are all so much together, but we are all dying of loneliness",
+  "The soul that sees beauty may sometimes walk alone" ,
+  "It is strange to be known so universally and yet to be so lonely",
+  "The greatest pain that comes from love is loving someone you can never have"
+];
+
+const friendsQuotes = [
+  "A friend is someone who knows all about you and still loves you",
+  "A real friend is one who walks in when the rest of the world walks out" ,
+  "Friendship is the only cement that will ever hold the world together",
+  "Good friends, good books, and a sleepy conscience: this is the ideal life",
+  "True friendship comes when the silence between two people is comfortable",
+  "There is nothing on this earth more to be prized than true friendship",
+  "A friend is one who overlooks your broken fence and admires the flowers in your garden",
+  "Friends are the siblings God never gave us",
+  "A single rose can be my garden… a single friend, my world",
+  "Friendship is the golden thread that ties the heart of all the world"
+];
+
+const mentalityQuotes = [
+  "Your mindset determines your success",
+  "The mind is everything What you think you become",
+  "Success is a state of mind If you want success, start thinking of yourself as a success" ,
+  "It's not what happens to you, but how you react to it that matters",
+  "Your attitude, not your aptitude, will determine your altitude" ,
+  "Change your thoughts and you change your world",
+  "Your mind is a powerful thing When you fill it with positive thoughts, your life will start to change",
+  "Our life is what our thoughts make it" ,
+  "Believe you can and you're halfway there",
+  "The only limit to our realization of tomorrow will be our doubts of today" 
+];
+
+const realWorldQuotes = [
+  "The real world is much smaller than the imaginary" ,
+  "In the real world, the smartest people are people who make mistakes and learn In school, the smartest people don't make mistakes",
+  "Reality is that which, when you stop believing in it, doesn't go away" ,
+  "In the real world, you should either be the one who makes things happen or the one who benefits from what happens",
+  "Life is not a problem to be solved, but a reality to be experienced",
+  "Reality is merely an illusion, albeit a very persistent one" ,
+  "The real world doesn't reward perfectionists It rewards people who get things done",
+  "Do not dwell in the past, do not dream of the future, concentrate the mind on the present moment",
+  "Reality leaves a lot to the imagination",
+  "Sometimes the real world is the hardest place to be" 
+];
+
+const allQuotes = {
+  selfImprovementQuotes,
+  heartBrokenQuotes,
+  lonelyQuotes,
+  friendsQuotes,
+  mentalityQuotes,
+  realWorldQuotes
+};
+
+module.exports = allQuotes;
